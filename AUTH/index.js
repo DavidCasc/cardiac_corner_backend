ff1c30f5067dd73446ecff78463552c5b014ff2f
DavidCasc <50248611+DavidCasc@users.noreply.github.com>
2021-11-04 22:17:25 -0400

    moved to aws

--- AUTH/index.js
+++ AUTH/index.js
@@ -1,14 +1,84 @@
-const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
+const AWS = require('aws-sdk'); //Connection client to database
+const express = require('express'); //Routing Service
+const cluster = require('cluster'); //Enable concurrency
+const os = require('os'); //To gain access to the core count
 const app = express();
-const bcrypt = require('bcrypt');
-const validator = require("email-validator");
+const bcrypt = require('bcrypt'); //Encryption
+const validator = require("email-validator"); //Check for email
 const nodemailer = require("nodemailer");
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '../.env' }); //Get environment variables
+const jwt = require('jsonwebtoken'); //Get JWT
 
+//Create connection to the database
+AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env. AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
-const jwt = require('jsonwebtoken');
+const dynamoClient = new AWS.DynamoDB.DocumentClient;
+const USER_TABLE = "users";
+
+//Function to delete users
+const deleteUser = async (name) => {
+    const params = {
+        TableName: USER_TABLE,
+        Key:{
+            username: name
+        }
+    };
+    const users = await dynamoClient.delete(params).promise();
+    return users;
+}
+const getUsers = async ()=> {
+    const params = {
+        TableName: USER_TABLE,
+    };
+    const users = await dynamoClient.scan(params).promise();
+    return users;
+}
+
+//Function to find user
+const getUser = async (name)=> {
+    const params = {
+        TableName: USER_TABLE,
+        KeyConditionExpression: "#un = :n",
+        ExpressionAttributeNames:{
+            "#un": "username"
+        },
+        ExpressionAttributeValues: {
+           ":n": name
+        }
+    };
+    const users = await dynamoClient.query(params).promise();
+    return users;
+}
+//Validate user
+const validateUser = async (token) => {
+    const username = jwt.verify(token, process.env.EMAIL_SECRET).user;
+    user = await getUser(username)
+    user = user.Items[0]
+    user.verfication = true
+    const params = {
+        TableName: USER_TABLE,
+        Item: user
+    };
+    await dynamoClient.put(params).promise();
+    return
+}
+
+//Function to add user
+const addUser = async (username, password) => {
+    const params = {
+        TableName: USER_TABLE,
+        Item: {
+            "username": username,
+            "password": password,
+            "verfication": false
+        }
+    };
+    return await dynamoClient.put(params).promise();
+}
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -18,59 +88,55 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+//Start app
 app.use(express.json());
-var numCore;
-let refreshTokens = [];
-let users = [
-    {
-        username: "john",
-        password: "test",
-        confirmed: "true"
-    }
-];
 
-
+//Find the numbers of cores the api is going to use
 if (process.env.AUTH_MAX_CORES == true) {
     numCore = os.cpus().length;
 } else {
     numCore = process.env.AUTH_NUM_CORES;
 }
 
-app.get('/confirmation/:token', async (req, res) => {
+app.get("/users", async (req, res) => {
+    const users = await getUsers()
+    return res.send(users)
+})
+
+//Create an end point for the verification
+app.get('/confirm/:token', async (req, res) => {
     try {
-        const { user: { id } } = jwt.verify(req.params.token, EMAIL_SECRET);
-        //await models.User.update({ confirmed: true }, { where: { id } });
+        validateUser(req.params.token);
     } catch (e) {
         res.send('error');
     }
-
-    return res.redirect('http://localhost:3001/login');
+    return res.status(200).send("Thank you for registering! You can close this page");
 });
 
-app.post('/register', (req, res) => {
+//Create an end point register users
+app.post('/register', async (req, res) => {
     //get user information from the request
     const username = req.body.username;
     const password = req.body.password;
 
     //check database for username
-    const dbUser = users.find(user => user.username === username);
-
-    if (dbUser != null) {
+    const dbUser = await getUser(username);
+    //Return if found
+    if (dbUser.Count != 0) {
         return res.status(400).send('User already exists');
     }
 
     //Encrypt/hash the password
-
-    const hashPass = bcrypt.hash(password, 10);
+    const hashPass = await bcrypt.hash(password, 10);
 
     //Create the user
-    let user = {
-        username: username,
-        password: hashPass,
-        confirmed: false
-    };
-
-
+    try {
+        await addUser(username, hashPass);
+    }   catch (err){
+        console.log(err)
+        return res.status(400).json(err)
+    }
+    
     //Send email
     const data = { user: username };
     const emailToken = generateEmailToken(data);
@@ -86,38 +152,34 @@ app.post('/register', (req, res) => {
         subject: 'Confirm Email',
         html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
     });
-
-    users.push(user);
-
-
     res.sendStatus(200)
 })
 
-app.post('/login', (req, res) => {
+app.delete("/delete", (req, res) => {
+    deleteUser("casciano.david@gmail.com")
+    return res.send(200)
+})
+
+app.post('/login', async (req, res) => {
     //Authenticate user
     const username = req.body.username;
-    const userCheck = users.find(user => user.username === username)
-    if (userCheck == null) {
+    const user = await getUser(username)
+    if (user.Count == 0) {
         return res.status(400).send('Cannot find user')
     }
-    try {
-        if (bcrypt.compare(req.body.password, userCheck.password)) {
-            const user = { name: username };
-            const accessToken = generateAccessToken(user);
-
-            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-
-            refreshTokens.push(refreshToken);
-            return res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    try{
+        if (await(bcrypt.compare(req.body.password, user.Items[0].password))) {
+            const resUser = user.Items[0].username;
+            const accessToken = generateAccessToken(resUser);
+            console.log(accessToken)
+            const refreshToken = jwt.sign(resUser, process.env.REFRESH_TOKEN_SECRET)
+            return res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
         } else {
-            res.send('Not Allowed')
-        }
+            return res.status(405).send('Not Allowed')
+        } 
     } catch {
-        res.status(500).send()
+        return res.status(500).send("Error Occurred")
     }
-
-
-
 })
 
 app.post('/token', (req, res) => {
@@ -149,11 +211,11 @@ app.delete('/logout', (req, res) => {
 })
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+    return jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
 }
 
 function generateEmailToken(data) {
-    return jwt.sign(data, process.env.EMAIL_SECRET, { expiresIn: '1d' });
+    return jwt.sign(data, process.env.EMAIL_SECRET, { expiresIn: '1y' });
 }
 
 if (cluster.isMaster) {
@@ -171,6 +233,6 @@ if (cluster.isMaster) {
     });
 } else {
     app.listen(process.env.AUTH_PORT, () =>
-        console.log(`Process ${process.pid} listening on port ${process.env.AUTH_PORT}`)
+        console.log(`Process ${process.pid} on port ${process.env.AUTH_PORT}`)
     );
 }
